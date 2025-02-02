7ca5133e8ca213ed4f52c7783cfe1e73bda2bb1d
Andrea2511 <andreittha4507@gmail.com>
2025-02-02 17:19:10 -0500

    feat: modify application to pause threads and display prime count every 5000 milliseconds
    - Modify `Control` class to pause all threads every 5000 milliseconds.
    - Display the number of primes found so far.
    - Require user to press ENTER to resume thread execution.

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -5,6 +5,8 @@
  */
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 /**
  *
  */
@@ -17,27 +19,73 @@ public class Control extends Thread {
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
-    
+    private boolean pause = false;
+
+
     private Control() {
+        //Create the threads with an empty constructor using start() method
         super();
         this.pft = new  PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+        for(i = 0; i < NTHREADS - 1; i++) {
+
+            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA, this);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1, this);
     }
     
     public static Control newControl() {
         return new Control();
     }
 
+    public boolean isPaused() {return pause;}
+
+    public synchronized void pauseThreads() {
+        pause = true;
+    }
+
+    public synchronized void resumeThread() {
+        pause = false;
+        notifyAll();
+    }
+
+    /**
+     * Run the threads and sleep the thread for 5000 milliseconds, showing the total of prime numbers found,
+     * waiting until the user press enter
+     * @throws InterruptedException
+     */
     @Override
     public void run() {
+
         for(int i = 0;i < NTHREADS;i++ ) {
             pft[i].start();
+        }
+
+        //Wait for enter
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            try {
+                Thread.sleep(TMILISECONDS);
+                pauseThreads();
+
+                int totalPrimes = 0;
+                for(PrimeFinderThread thread : pft) {
+                    totalPrimes += thread.getPrimes().size();
+                }
+
+                System.out.println("Total primes found: " + totalPrimes);
+                System.out.println("Press enter to continue...");
+
+                scanner.nextLine();
+                resumeThread();
+
+            } catch (InterruptedException e) {
+
+                throw new RuntimeException(e);
+            }
         }
     }
     
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -5,41 +5,65 @@ import java.util.List;
 
 public class PrimeFinderThread extends Thread{
 
-	
-	int a,b;
-	
+	int a, b;
 	private List<Integer> primes;
+    private final Control control;
 	
-	public PrimeFinderThread(int a, int b) {
+	public PrimeFinderThread(int a, int b, Control control) {
 		super();
-                this.primes = new LinkedList<>();
+        this.primes = new LinkedList<>();
 		this.a = a;
 		this.b = b;
+        this.control = control;
 	}
 
-        @Override
+    @Override
 	public void run(){
-            for (int i= a;i < b;i++){						
+            for (int i= a;i < b;i++){
+                //avoid race condition, using synchronized block (just one thread can access to the block)
+                synchronized (control) {
+                    while (control.isPaused()) {
+                        try {
+                            control.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
                 if (isPrime(i)){
                     primes.add(i);
                     System.out.println(i);
                 }
             }
 	}
-	
+
+    /**
+     * Verify if a number is prime
+     * @param n number
+     * @return boolean with the verification if the number is prime
+     */
 	boolean isPrime(int n) {
 	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
+
+        if (n > 2) {
+            ans = n%2 != 0;
+
+            //if n is divisible by any odd number then it is not prime
+            for(int i = 3;ans && i*i <= n; i+=2 ) {
+                ans = n % i != 0;
             }
+
+        } else {
+            ans = n == 2;
+        }
+
 	    return ans;
 	}
 
+    /**
+     * list prime numbers
+     * @return list of prime numbers
+     */
 	public List<Integer> getPrimes() {
 		return primes;
 	}
